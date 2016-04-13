38a9d6b7179782fec7463622817382bc27e5ce28
fx35792 <fx35792@163.com>
2016-04-13 09:17:26 +0800

    modify file

--- day01/controllersSpec.js
+++
@@ -1,9 +0,0 @@
-describe('PhoneCat controller',function(){
-	describe('PhoneListCtrl',function(){
-		it('should create "phones" model with 3 phones',function(){
-			var scope = {},
-			ctrl = new PhoneListCtrl(scope);
-			expect(scope.phones.lenght).toBe(3);
-		});
-	});
-});
